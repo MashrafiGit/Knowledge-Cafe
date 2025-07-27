76724d495544d2539c3d8be4883ff09e897e6ec5
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-28 01:24:50 +0600

    updated bookmark design

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 const Bookmark = ({ bookmark }) => {
   const { title } = bookmark;
   return (
-    <div className="bg-slate-200 p-3 my-4 rounded-xl">
-      <h3 className="text-2xl">{title}</h3>
+    <div className="bg-slate-50 p-3 my-4 rounded-md">
+      <h3 className="text-xl">{title}</h3>
     </div>
   );
 };
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,7 +3,7 @@ import Bookmark from "../Bookmark/Bookmark";
 
 const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
-    <div className="md:w-1/3 bg-gray-300 ml-4 my-2 py-4 px-3 rounded">
+    <div className="md:w-1/3 bg-gray-200 ml-4 my-2 py-4 px-3 rounded">
       <div>
         <h3 className="text-3xl">Reading Time: {readingTime}</h3>
       </div>
